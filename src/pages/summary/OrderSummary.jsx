b97fd9fc7eb0ff6b022d7ed20491b3a471e1ff66
Yu MIAO <yuuu.miao@gmail.com>
2021-06-15 03:00:39 +0200

    setup tests config

---
+++ src/pages/summary/OrderSummary.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class OrderSummary extends Component {
+  render() {
+    return <div className="OrderSummary"></div>;
+  }
+}
+
+export default OrderSummary;
